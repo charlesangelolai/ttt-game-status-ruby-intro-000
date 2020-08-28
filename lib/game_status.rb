d424d4275ae119b11af712ea69774d4129cf9afd
Charles Lai <charlesangeloplai@gmail.com>
2020-08-28 18:04:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,21 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+# won method : returns true if there is a winner; else if not
+def won?(board)
+  each.WIN_COMBINATIONS do |win_index|
+    win_index_1 = win_index[0]
+    win_index_2 = win_index[1]
+    win_index_3 = win_index[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position = "X" && position 2 == "X" && position_3 == "X"
+      return win_combination
+    else
+      false
+    end
+end
